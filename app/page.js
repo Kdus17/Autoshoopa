2e6b246f5a9ddb99af1ac4bf2460a41571ff0742
Kdus17 <kidus2460@gmail.com>
2025-04-18 19:55:50 +0300

    sidebar

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import ProductExplore from "@/components/ProductExplore";
 import BilingDetails from "@/components/BillingDetails";
 import Footer from "@/components/Footer";
@@ -9,21 +10,40 @@ import SearchBar from "@/components/Search";
 import AddProduct from "@/components/AddProduct";
 import OrderConformation from "@/components/OrderConformation";
 import CheckOut from "@/components/CheckOut";
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <SearchBar />
-      <AddProduct />
-      <CheckOut />
-      <ProductExplore />
-      <Filters />
-      <Contact />
-      <Track />
-      <BilingDetails />
-      <OrderConformation />
-      <TrackOrder />
-      <Footer />
+      <button
+        onClick={() => setOpen(!open)}
+        className="fixed top-4 right-4 z-100 bg-indigo-600 text-white px-4 py-2 rounded "
+      >
+        {open ? "Close Cart" : "Open Cart"}
+      </button>
+      {open && (
+        <div>
+          <CheckOut />
+        </div>
+      )}
+      <div
+        className={`transition-all duration-300 ${
+          open ? "filter blur-sm" : ""
+        }`}
+      >
+        <AddProduct />
+
+        <ProductExplore />
+        <Filters />
+        <Contact />
+        <Track />
+        <BilingDetails />
+        <OrderConformation />
+        <TrackOrder />
+        <Footer />
+      </div>
     </>
   );
 }
