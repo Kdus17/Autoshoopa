1b2af1984631652853b1ed44ecd63fe0be6adfc1
Kdus17 <kidus2460@gmail.com>
2025-05-03 08:49:10 +0300

    minor fix

--- app/Confirmation/page.js
+++ app/Confirmation/page.js
@@ -1,5 +1,10 @@
 import OrderConformation from "@/components/OrderConformation";
+import { Suspense } from "react";
 
 export default function Confirmation() {
-  return <OrderConformation />;
+  return (
+    <Suspense>
+      <OrderConformation />
+    </Suspense>
+  );
 }
